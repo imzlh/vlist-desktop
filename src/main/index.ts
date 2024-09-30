6237c199cb37368b3a818c524353fd12e109cede
iz <2131601562@qq.com>
2024-10-01 07:07:44 +0800

    http range support

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import { createServer } from 'http'
 import { createReadStream } from 'fs'
 import { FakeNgxResponse } from './polyfill'
 import API from './server';
+import { stat } from 'fs/promises'
 
 app.whenReady().then(() => {
     electronApp.setAppUserModelId('com.electron')
@@ -55,6 +56,7 @@ ipcMain.addListener('create-session', (_: Event, search: string) => {
         width: 1200,
         height: 800,
         autoHideMenuBar: true,
+        icon: require.resolve('@imzlh/vlist/dist/favicon.ico'),
         webPreferences: {
             nodeIntegration: true,
             sandbox: false,
@@ -63,7 +65,7 @@ ipcMain.addListener('create-session', (_: Event, search: string) => {
             preload: join(__dirname, '../preload/index.js')
         }
     });
-    newWin.loadFile('node_modules/@imzlh/vlist/dist/index.html', {
+    newWin.loadFile(require.resolve('@imzlh/vlist/dist/index.html'), {
         search
     });
     windows.push(newWin);
@@ -71,25 +73,63 @@ ipcMain.addListener('create-session', (_: Event, search: string) => {
 })
 ipcMain.addListener('create-server', (_, path: string) => {
     // 创建服务器
-    const server = createServer((req, res) => {
+    const server = createServer(async (req, res) => {
         const url = new URL(req.url!, `http://${req.headers.host}`);
         console.log(req.method, url.pathname);
         if (url.pathname === '/@api/') {
             const vhandle = new FakeNgxResponse(res);
-            vhandle.__waitReady()
-                .then(() => API(vhandle, path))
-                .catch(e => console.error(e));
+            try{
+                await vhandle.__waitReady()
+                await API(vhandle, path);
+            }catch(e){
+                console.error(e);
+                if(!res.writableFinished)
+                    vhandle.return(403, String(e));
+            }
         }else{
             // 设置静态文件目录
-            const filePath = join(path, url.pathname);
+            const filePath = join(path, decodeURIComponent(url.pathname));
+
+            let stats;
+            try{
+                stats = await stat(filePath);
+            }catch(e){
+                res.statusCode = 404;
+                res.end(String(e));
+                return;
+            }
+
+            const range = [0, stats.size];
+            if(req.headers.range){
+                const match = req.headers.range.match(/bytes=(\d+)-(\d*)/);
+                if(match){
+                    if(match[1]) range[0] = parseInt(match[1]);
+                    if(match[2]) range[1] = parseInt(match[2]) +1;
+
+                    if(range[0] < 0 || range[1] > stats.size || range[0] >= range[1]){
+                        res.statusCode = 416;
+                        return res.end('Requested Range Not Satisfiable');
+                    }
+
+                    res.statusCode = 206;
+                    res.setHeader('Content-Range', `bytes ${range[0]}-${range[1]-1}/${stats.size}`);
+                }else{
+                    res.statusCode = 416;
+                    return res.end('invalid range format');
+                }
+            }
             
             try{
                 res.statusCode = 200;
+                res.setHeader('Content-Length', range[1] - range[0]);
                 res.setHeader('Content-Type', 'application/octet-stream');
-                createReadStream(filePath).pipe(res);
+                createReadStream(filePath, {
+                    "start": range[0],
+                    "end": range[1]
+                }).pipe(res);
             }catch(e){
                 res.statusCode = 404;
-                res.end();
+                res.end(String(e));
             }
         }
     });
@@ -112,7 +152,7 @@ ipcMain.addListener('create-server', (_, path: string) => {
             preload: join(__dirname, '../preload/index.js')
         }
     });
-    newWin.loadFile('node_modules/@imzlh/vlist/dist/index.html', {
+    newWin.loadFile(require.resolve('@imzlh/vlist/dist/index.html'), {
         "search": `?api=http://${addrstr}/@api/&proxy=http://${addrstr}`
     });
     newWin.on('closed', () => server.close());
